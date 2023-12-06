c83df4cef3b8c748b78a67695440573587675fc7
youngmikec <michaelozor15@gmail.com>
2023-12-06 06:00:06 +0100

    Progress: working on home welcome board UI

--- components/home-screen-comps/WelcomeBoard.tsx
+++ components/home-screen-comps/WelcomeBoard.tsx
@@ -6,6 +6,8 @@ const WelcomeBoard = () => {
         <>
             <View style={style.container}>
                 <Image style={style.vectorImg} source={require('../../assets/images/vector-bg.png')} />
+                <Text style={style.welcomeText}>Welcome Back Michael</Text>
+                <Text style={style.text}>Did you know that taking a glass of water early in the morning can help make you stay happy energetic for most of the day?</Text>
             </View>
         </>
     )
@@ -23,7 +25,17 @@ const style = StyleSheet.create({
     vectorImg: {
         position: 'absolute',
         top: -5,
-        right: -3
+        right: -3,
+    },
+    welcomeText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
     }
 })
 
